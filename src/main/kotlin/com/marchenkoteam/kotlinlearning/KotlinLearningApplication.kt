d041eafd6a23ab5fe729b55278f562bd41780ac2
IlyaZakharchenko <zakharchenkoilyajob@gmail.com>
2018-12-03 00:17:19 +0300

    Fix cross origin.

--- src/main/kotlin/com/marchenkoteam/kotlinlearning/KotlinLearningApplication.kt
+++ src/main/kotlin/com/marchenkoteam/kotlinlearning/KotlinLearningApplication.kt
@@ -2,10 +2,12 @@ package com.marchenkoteam.kotlinlearning
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.CrossOrigin
 import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 @SpringBootApplication
 @EnableSwagger2
+@CrossOrigin
 class KotlinLearningApplication
 
 fun main(args: Array<String>) {
--- src/main/kotlin/com/marchenkoteam/kotlinlearning/controllers/TestController.kt
+++ src/main/kotlin/com/marchenkoteam/kotlinlearning/controllers/TestController.kt
@@ -4,11 +4,9 @@ import com.marchenkoteam.kotlinlearning.dto.TestDto
 import com.marchenkoteam.kotlinlearning.models.Test
 import com.marchenkoteam.kotlinlearning.services.TestService
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
+@CrossOrigin
 @RestController
 @RequestMapping("/tests")
 class TestController @Autowired constructor(private val testService: TestService) {
