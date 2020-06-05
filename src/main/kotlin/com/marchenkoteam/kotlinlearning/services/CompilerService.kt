24c582146cd21637f313368db31ac9cc7b6a5593
Kiselev, Alexandr V <kis.alex97@gmail.com>
2020-06-05 17:38:38 +0300

    feature: added compile test files to jar

--- src/main/kotlin/com/marchenkoteam/kotlinlearning/services/CompilerService.kt
+++ src/main/kotlin/com/marchenkoteam/kotlinlearning/services/CompilerService.kt
@@ -14,23 +14,30 @@ class CompilerService @Autowired constructor(private val authService: AuthServic
     private lateinit var userFilesDir: String
 
     fun runTest(testId: String, testAnswer: TestAnswer) {
-        createFile(testId, testAnswer.code)
+        val filename = createFile(testId, testAnswer.code)
+        compileFile(filename)
     }
 
-    private fun createFile(testId: String, code: String) {
+    private fun createFile(testId: String, code: String): String {
         val currentUserId = authService.getCurrentUser().id ?: throw IllegalArgumentException("userId must not be null")
-        val testFile = File(generateFilename(currentUserId, testId))
+        val filename = generateFilename(testId)
+        val testFile = File("${getUserDirPath(currentUserId)}/$filename.kt")
         testFile.createNewFile()
         val fileWriter = PrintWriter(testFile)
         fileWriter.use {
             it.write(code)
         }
+        return filename
     }
 
-    private fun generateFilename(userId: String, testId: String): String {
-        val dirPath = "${userFilesDir}/${userId}"
+    private fun generateFilename(testId: String): String {
+        return "${testId}_${System.currentTimeMillis()}"
+    }
+
+    private fun getUserDirPath(userId: String): String {
+        val dirPath = "$userFilesDir/$userId"
         checkDir(dirPath)
-        return "${dirPath}/${testId}_${System.currentTimeMillis()}.kt"
+        return dirPath
     }
 
     private fun checkDir(dirPath: String) {
@@ -38,5 +45,15 @@ class CompilerService @Autowired constructor(private val authService: AuthServic
         if (!dir.exists()) {
             dir.mkdirs()
         }
+    }
+
+    private fun compileFile(filename: String) {
+        val runtime = Runtime.getRuntime()
+        val currentUserId = authService.getCurrentUser().id
+                ?: throw java.lang.IllegalArgumentException("There is no id in user entity")
+        val file = File(getUserDirPath(currentUserId))
+        println(file.exists())
+        val process = runtime.exec("cmd.exe /c kotlinc $filename.kt -d $filename.jar", null, file)
+        process.waitFor()
     }
 }
